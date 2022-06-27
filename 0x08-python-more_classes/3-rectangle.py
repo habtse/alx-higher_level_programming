f4201df37c4cbc2d55d4bc5d64e51d410c131652
root <habtetsegaye19@gmail.com>
2022-06-27 14:28:52 -0700

    error handled

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -58,7 +58,7 @@ class Rectangle:
         """
         if self.__width == 0 and self.__height == 0:
             return ("")
-         rect = []
+        rect = []
         for i in range(self.__height):
             [rect.append('#') for j in range(self.__width)]
             if i != self.__height - 1:
