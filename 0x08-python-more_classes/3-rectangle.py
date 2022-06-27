8df5212852c41101b05186c6e9cac47239eb100e
root <habtetsegaye19@gmail.com>
2022-06-27 14:37:30 -0700

    error handled

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -53,12 +53,11 @@ class Rectangle:
     
     def __str__(self):
         """Return the printable representation of the Rectangle.
-
         Represents the rectangle with the # character.
         """
         if self.__width == 0 or self.__height == 0:
-            return ("") 
-        
+            return ("")
+
         rect = []
         for i in range(self.__height):
             [rect.append('#') for j in range(self.__width)]
